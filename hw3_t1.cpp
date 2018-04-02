a89696f6b71fce94117e3153041935c428cb52e9
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2018-04-02 18:59:28 -0400

    almost there

--- hw3_t1.cpp
+++ hw3_t1.cpp
@@ -6,9 +6,6 @@
 #include <sstream>
 #include <string>
 
-#include "clang/ASTMatchers/ASTMatchFinder.h"
-#include "clang/ASTMatchers/ASTMatchers.h"
-
 #include "clang/AST/AST.h"
 #include "clang/AST/ASTConsumer.h"
 #include "clang/AST/RecursiveASTVisitor.h"
@@ -22,57 +19,179 @@
 #include "llvm/Support/raw_ostream.h"
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <string>
 #include <vector>
+
 using namespace std;
 using namespace clang;
 using namespace clang::driver;
 using namespace clang::tooling;
-using namespace llvm;
-using namespace clang::ast_matchers;
 
-class FunctionPrinter : public MatchFinder::MatchCallback {
-public:
-  virtual void run(const MatchFinder::MatchResult &Result) {
-
-    auto callee = Result.Nodes.getNodeAs<clang::CallExpr>("callee");
-    auto caller = Result.Nodes.getNodeAs<clang::CallExpr>("caller");
-
-    std::string callee_str =
-        callee->getDirectCallee()->getNameInfo().getName().getAsString();
-
-    std::string caller_str =
-        caller->getDirectCallee()->getNameInfo().getName().getAsString();
-    std::cout << callee_str << std::endl;
-    /*
-
-   std::cout << caller_str << std::endl;
-   */
-  }
-};
+vector<IfStmt *> ifstmtvector;
+vector<ForStmt *> forstmtvector;
+std::map<FunctionDecl, vector<FunctionDecl>> functionDecl;
+vector<Stmt *> stmtvector;
+vector<FunctionDecl> functiondeclvector;
+vector<CallExpr *> callexprvector;
 
 static llvm::cl::OptionCategory ToolingSampleCategory("Tooling Sample");
+
+// get the source code of the specific parts of AST
+template <typename T>
+static std::string getText(const SourceManager &SourceManager, const T &Node) {
+  SourceLocation StartSpellingLocation =
+      SourceManager.getSpellingLoc(Node.getLocStart());
+  SourceLocation EndSpellingLocation =
+      SourceManager.getSpellingLoc(Node.getLocEnd());
+  if (!StartSpellingLocation.isValid() || !EndSpellingLocation.isValid()) {
+    return std::string();
+  }
+  bool Invalid = true;
+  const char *Text =
+      SourceManager.getCharacterData(StartSpellingLocation, &Invalid);
+  if (Invalid) {
+    return std::string();
+  }
+  std::pair<FileID, unsigned> Start =
+      SourceManager.getDecomposedLoc(StartSpellingLocation);
+  std::pair<FileID, unsigned> End =
+      SourceManager.getDecomposedLoc(Lexer::getLocForEndOfToken(
+          EndSpellingLocation, 0, SourceManager, LangOptions()));
+  if (Start.first != End.first) {
+    // Start and end are in different files.
+    return std::string();
+  }
+  if (End.second < Start.second) {
+    // Shuffling text with macros may cause this.
+    return std::string();
+  }
+  return std::string(Text, End.second - Start.second);
+}
+
+// By implementing RecursiveASTVisitor, we can specify which AST nodes
+// we're interested in by overriding relevant methods.
+class MyASTVisitor : public RecursiveASTVisitor<MyASTVisitor> {
+public:
+  MyASTVisitor(Rewriter &R) : TheRewriter(R) {}
+
+  bool VisitStmt(Stmt *st) {
+    // Only function definitions (with bodies), not declarations.
+    if (CallExpr *call = dyn_cast<CallExpr>(st)) {
+      callexprvector.push_back(call);
+    }
+    return true;
+  }
+
+private:
+  Rewriter &TheRewriter;
+};
+
+// Implementation of the ASTConsumer interface for reading an AST produced
+// by the Clang parser.
+class MyASTConsumer : public ASTConsumer {
+public:
+  MyASTConsumer(Rewriter &R) : Visitor(R) {}
+
+  // Override the method that gets called for each parsed top-level
+  // declaration.
+  bool HandleTopLevelDecl(DeclGroupRef DR) override {
+    for (DeclGroupRef::iterator b = DR.begin(), e = DR.end(); b != e; ++b) {
+      // Traverse the declaration using our AST visitor.
+      Visitor.TraverseDecl(*b);
+      //(*b)->dump();
+    }
+    return true;
+  }
+
+private:
+  MyASTVisitor Visitor;
+};
+
+const clang::Decl *get_DeclContext_from_Stmt(const clang::Stmt &stmt) {
+  auto it = ASTContext->getParents(stmt).begin();
+
+  if (it == ASTContext->getParents(stmt).end())
+    return nullptr;
+
+  const clang::Decl *aDecl = it->get<clang::Decl>();
+  if (aDecl)
+    return aDecl;
+
+  const clang::Stmt *aStmt = it->get<clang::Stmt>();
+  if (aStmt)
+    return get_DeclContext_from_Stmt(*aStmt);
+
+  return nullptr;
+}
+
+// For each source file provided to the tool, a new FrontendAction is created.
+class MyFrontendAction : public ASTFrontendAction {
+public:
+  MyFrontendAction() {}
+  void EndSourceFileAction() override {
+    SourceManager &SM = TheRewriter.getSourceMgr();
+    llvm::errs() << "** EndSourceFileAction for: "
+                 << SM.getFileEntryForID(SM.getMainFileID())->getName() << "\n";
+
+    // handle each function definition
+    for (unsigned exprid = 0; exprid < callexprvector.size(); exprid++) {
+      ofstream myfile;
+      myfile.open("call_graph.dot");
+      cout << ("call_graph.dot") << endl;
+      myfile << "digraph unnamed {\n";
+
+      while (true) {
+        // get parents
+        const auto &parents = Context->getParents(*callexprvector[exprid]);
+        if (parents.empty()) {
+          llvm::errs() << "Can not find parent\n";
+        }
+        llvm::errs() << "find parent size=" << parents.size() << "\n";
+
+        for (int j = 0; j < parents.size(); j++) {
+          const Stmt *ST = parents[j].get<Stmt>();
+
+          //	if (FunctionDecl *decl= dyn_cast<FunctionDec>(st)) {
+
+          if (ISA(FunctionDecl, ST)) {
+          }
+          // const clang::Decl *aDecl = parents->get<clang::Decl>();
+          //	  if(aDecl)
+        }
+      }
+
+      myfile << "Node1 [shape=record,label=\"{ [(ENTRY)]\\l}\"];\n";
+
+      myfile << "Node2 [shape=record,label=\"Dummy Node\"]\n";
+      myfile << "Node1 -> Node2;\n";
+
+      myfile << "}\n";
+      myfile.close();
+    }
+  }
+
+  std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI,
+                                                 StringRef file) override {
+    llvm::errs() << "** Creating AST consumer for: " << file << "\n";
+    TheRewriter.setSourceMgr(CI.getSourceManager(), CI.getLangOpts());
+    Context = &CI.getASTContext();
+    return llvm::make_unique<MyASTConsumer>(TheRewriter);
+  }
+
+private:
+  Rewriter TheRewriter;
+  ASTContext *Context;
+};
 
 int main(int argc, const char **argv) {
   CommonOptionsParser op(argc, argv, ToolingSampleCategory);
   ClangTool Tool(op.getCompilations(), op.getSourcePathList());
 
-  MatchFinder Finder;
-  FunctionPrinter Printer;
-  /* Finder.addMatcher(forStmt(hasLoopInit(declStmt(hasSingleDecl(varDecl(
-                                 hasInitializer(integerLiteral(equals(0))))))))
-                         .bind("forLoop"),
-                     &Printer);
- */
-
-  Finder.addMatcher(callExpr(callee(functionDecl()),
-                             hasAncestor(functionDecl().bind("caller")))
-                        .bind("callee"),
-                    &Printer);
-  /*
-    Finder.addMatcher(callExpr(callee(hasDescendant(declRefExpr(
-                                   to(functionDecl().bind("callee"))))))
-                          .bind("caller"));
-  */
-  return Tool.run(newFrontendActionFactory(&Finder).get());
+  // ClangTool::run accepts a FrontendActionFactory, which is then used to
+  // create new objects implementing the FrontendAction interface. Here we use
+  // the helper newFrontendActionFactory to create a default factory that will
+  // return a new MyFrontendAction object every time.
+  // To further customize this, we could create our own factory class.
+  return Tool.run(newFrontendActionFactory<MyFrontendAction>().get());
 }
---
+++ hw3_t2.cpp
@@ -0,0 +1,130 @@
+//== TraversalChecker.cpp -------------------------------------- -*- C++ -*--=//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// These checkers print various aspects of the ExprEngine's traversal of the CFG
+// as it builds the ExplodedGraph.
+//
+//===----------------------------------------------------------------------===//
+#include "ClangSACheckers.h"
+#include "clang/AST/ParentMap.h"
+#include "clang/AST/StmtObjC.h"
+#include "clang/StaticAnalyzer/Core/Checker.h"
+#include "clang/StaticAnalyzer/Core/CheckerManager.h"
+#include "clang/StaticAnalyzer/Core/PathSensitive/CallEvent.h"
+#include "clang/StaticAnalyzer/Core/PathSensitive/CheckerContext.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace clang;
+using namespace ento;
+
+// create a state specifier
+struct ProgramPairState {
+public:
+  int Lock;
+  int MyMalloc;
+
+  ProgramPairState(int lock, int myMalloc) : Lock(lock), MyMalloc(myMalloc) {}
+  bool operator==(const ProgramPairState &X) const {
+    return Lock == X.Lock && MyMalloc == X.MyMalloc;
+  }
+
+  void Profile(llvm::FoldingSetNodeID &ID) const {
+    ID.AddInteger(Lock);
+    ID.AddInteger(MyMalloc);
+  }
+};
+
+namespace {
+class PairFunctionChecker : public Checker<check::PreCall, check::PostCall> {
+public:
+  PairFunctionChecker();
+  void checkPreCall(const CallEvent &Call, CheckerContext &C) const;
+  void checkPostCall(const CallEvent &Call, CheckerContext &C) const;
+};
+} // namespace
+
+REGISTER_MAP_WITH_PROGRAMSTATE(PairMap, Decl *, ProgramPairState)
+
+void PairFunctionChecker::checkPreCall(const CallEvent &Call,
+                                       CheckerContext &C) const {
+
+  ProgramStateRef State = C.getState();
+  auto caller = C.getCurrentAnalysisDeclContext()->getDecl();
+  auto callee = Call.getDecl();
+
+  // current state
+  ProgramPairState myState = state->get<PairMap>(caller);
+
+  if (Call.isCalled("lock")) {
+    state = state->set<PairMap>(
+        caller, ProgramPairState(++myState.Lock, myState.MyMalloc));
+  }
+
+  if (Call.isCalled("unlock")) {
+    state = state->set<PairMap>(
+        caller, ProgramPairState(--myState.Lock, myState.MyMalloc));
+    if (state->get<PairMap>(caller).Lock < 0)
+      llvm::outs() << caller->getAsFunction()->getNameInfo().getAsString()
+                   << ": False";
+  }
+
+  if (Call.isCalled("mymalloc")) {
+    state = state->set<PairMap>(
+        caller, ProgramPairState(myState.Lock, ++myState.MyMalloc));
+  }
+
+  if (Call.isCalled("myfree")) {
+    state = state->set<PairMap>(
+        caller, ProgramPairState(myState.Lock, --myState.MyMalloc));
+
+    if (state->get<PairMap>(caller).MyMalloc < 0)
+      llvm::outs() << caller->getAsFunction()->getNameInfo().getAsString()
+                   << ": False";
+  }
+
+  C.addTransition(state);
+}
+
+void CallDumper::checkPostCall(const CallEvent &Call, CheckerContext &C) const {
+
+  ProgramStateRef State = C.getState();
+  auto caller = C.getCurrentAnalysisDeclContext()->getDecl();
+  auto callee = Call.getDecl();
+
+  ProgramPairState myState = state->get<PairMap>(caller);
+
+  if (Call.isCalled("lock")) {
+    if (state->get<PairMap>(caller).Lock != 0)
+      llvm::outs() << caller->getAsFunction()->getNameInfo().getAsString()
+                   << ": False";
+  }
+
+  if (Call.isCalled("unlock")) {
+    if (state->get<PairMap>(caller).Lock != 0)
+      llvm::outs() << caller->getAsFunction()->getNameInfo().getAsString()
+                   << ": False";
+  }
+
+  if (Call.isCalled("mymalloc")) {
+    if (state->get<PairMap>(caller).MyMalloc != 0)
+      llvm::outs() << caller->getAsFunction()->getNameInfo().getAsString()
+                   << ": False";
+  }
+
+  if (Call.isCalled("myfree")) {
+    if (state->get<PairMap>(caller).MyMalloc != 0)
+      llvm::outs() << caller->getAsFunction()->getNameInfo().getAsString()
+                   << ": False";
+  }
+  C.addTransition(state);
+}
+
+void ento::registerCallDumper(CheckerManager &mgr) {
+  mgr.registerChecker<CallDumper>();
+}
